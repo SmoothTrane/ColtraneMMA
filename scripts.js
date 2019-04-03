c562fb225603df2c5010165737892622e7d857f0
Gary Coltrane <coltranegary@gmail.com>
2019-04-03 02:32:55 -0400

    added updates to the design:

---
+++ scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("#about").click(function(e){
+    $("html").animate({
+        scrollTop: $("html").offset().top
+    }, 1000);
+  })
+  $("#model").click(function(e){
+    $("html").animate({
+        scrollTop: $(".page-wrapper").offset().top
+    }, 1000);
+  })
+  $("#disciplines").click(function(e){
+    $("html").animate({
+        scrollTop: $(".decoy-hack").offset().top
+    }, 1000);
+  });
+  $("#contact").click(function(e){
+    $("html").animate({
+        scrollTop: $(".contact-footer").offset().top
+    }, 1000);
+  })
+});
